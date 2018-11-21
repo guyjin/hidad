b7e543fe5348641cb87073444cead7ba2b75c471
Ben Vaughan <benjamin.vaughan@gmail.com>
2018-11-21 16:45:05 -0700

    genders!
    added genders to the json.  updated template to use correct pronoun.

--- js/hidad.js
+++ js/hidad.js
@@ -8,21 +8,24 @@ var dads = [
         born:"12/12/1940",
         died:"05/28/2007",
         age:"",
-        elapsed:""
+        elapsed:"",
+        isMale: true
     },
     {
         name:"Jean",
         born:"04/24/1945",
         died:"10/12/2000",
         age:"",
-        elapsed:""
+        elapsed:"",
+        isMale: true
     },
     {
         name:"Ernie",
         born:"03/01/1924",
         died:"07/08/1996",
         age:"",
-        elapsed:""
+        elapsed:"",
+        isMale: true
     }
 ];
 
@@ -32,21 +35,32 @@ var others = [
         born:"10/5/1929",
         died:"12/21/2015",
         age:"",
-        elapsed:""
+        elapsed:"",
+        isMale: false
+    },
+    {
+        name: "Kenny",
+        born:"04/27/1948",
+        died:"11/6/2015",
+        age:"",
+        elapsed:"",
+        isMale: true
     },
     {
         name: "DeeAnne",
         born:"03/07/1953",
         died:"10/09/2008",
         age:"",
-        elapsed:""
+        elapsed:"",
+        isMale: false
     },
     {
         name: "Michael",
         born:"05/05/1971",
         died:"10/08/1995",
         age:"",
-        elapsed:""
+        elapsed:"",
+        isMale: true
     }
 ];
 
@@ -93,7 +107,7 @@ function assignResults() {
 }
 
 function renderTemplate(person) {
-    var template = '<div class="person"><div class="bio"><h3>{{name}}</h3><p>Born {{born}}, died {{died}}.</p><p>He was <span class="age">{{age}}</span> years old.</p></div><div class="elapsed">It\'s been about <span class="elapsed">{{elapsed}}</span> since he died.</div></div>'
+    var template = '<div class="person"><div class="bio"><h3>{{name}}</h3><p>Born {{born}}, died {{died}}.</p><p>{{#isMale}}He{{/isMale}}{{^isMale}}She{{/isMale}} was <span class="age">{{age}}</span> years old.</p></div><div class="elapsed">It\'s been about <span class="elapsed">{{elapsed}}</span> since {{^isMale}}s{{/isMale}}he died.</div></div>'
     var target = $('.people');
     $(target).append(Mustache.render(template, person));
 }
