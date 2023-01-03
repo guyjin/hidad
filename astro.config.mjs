a514d0c3a9fbbab91fceb3c2b309eec8165da9aa
Ben Vaughan <benjamin.vaughan@gmail.com>
2023-01-02 21:00:20 -0700

    Got 50 year tape based on current year.  Added nesting plugin for Tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,14 @@ import react from "@astrojs/react";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import nesting from "tailwindcss/nesting";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  style: {
+    postcss: {
+      plugins: [nesting()],
+    },
+  },
 });
